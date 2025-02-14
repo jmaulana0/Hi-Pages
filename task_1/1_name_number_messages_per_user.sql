b4e4ee488a1ae93ace2ca75aff59ce9a923caaad
jmaulana0 <jmaulana@gmail.com>
2025-02-14 15:24:59 +1100

    Update 1_name_number_messages_per_user.sql

--- task_1/1_name_number_messages_per_user.sql
+++ task_1/1_name_number_messages_per_user.sql
@@ -2,7 +2,6 @@ SELECT
     u.name AS user_name,
     COUNT(m.MessageID) AS messages_sent
 FROM Users u
-WHERE 1=1
 JOIN Messages m ON u.UserID = m.UserIDSender
 GROUP BY u.UserID, u.name
-ORDER BY messages_sent DESC;
+ORDER BY COUNT(m.MessageID) DESC;
